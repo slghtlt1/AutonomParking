4868ef3df58c1a905094a9ac643a538d548b1917
slghtlt1 <auto.it.email@gmail.com>
2021-01-09 09:14:11 +0200

    Here i added all my files again

---
+++ src/main/java/db/ConnectionDB.java
@@ -0,0 +1,6 @@
+package db;
+
+public class ConnectionDB {
+       public static final String url = "jdbc:mysql://localhost:3306/Persoane";
+       public static final String driver = "com.mysql.jdbc.Driver";
+}
