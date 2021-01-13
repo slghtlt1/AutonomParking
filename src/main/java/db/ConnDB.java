600ed11ee9e03a3e917b0beed4c60e0cca5cc987
slghtlt1 <auto.it.email@gmail.com>
2021-01-13 14:35:04 +0200

    I've changed the user&pass to constants

--- src/main/java/db/ConnDB.java
+++ src/main/java/db/ConnDB.java
@@ -9,8 +9,10 @@ import java.sql.SQLException;
 public class ConnDB {
     //TODO:URL si Driver le fac constante si le mut intr-o clasa de constante si folosesc constantele din clasa aia
     public static Connection createConn() throws ClassNotFoundException, SQLException {
+        String user = "root";
+        String pass = "slghtlt1";
         Class.forName(DBUtil.driver);
-        return DriverManager.getConnection(DBUtil.url, "root", "slghtlt1");
+        return DriverManager.getConnection(DBUtil.url, user, pass);
     }
 
 //1
